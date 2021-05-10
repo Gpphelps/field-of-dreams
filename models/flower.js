03542f151016c93dda94336a57201f7bfc8c19b1
Gene Phelps <gpphelpsii@gmail.com>
2021-05-10 18:45:25 -0400

    Added code to flower.js, index.js, homepage.handlebars, and login.handlebars

---
+++ models/flower.js
@@ -0,0 +1,80 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Flower extends Model {}
+
+Flower.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        height: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            max: 10,
+            min: 1,
+        },
+        stem_width: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        max_curve: {
+           type: DataTypes.INTEGER,
+           allowNull: true,
+        },
+        max_variation: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        bulb_radius: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        bulb_color_R: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        bulb_color_G: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        bulb_color_B: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+        },
+        petal_color_variation: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        segments: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        segment_variation: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        petal_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        petal_scale: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        petal_shape: {
+            type: DataTypes.Sequelize.json,
+
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'flower',
+    }
+)
+
+module.exports = Flower;
