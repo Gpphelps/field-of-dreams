83021f5c162ac69f4a1b8e342396c0087ba3a93c
Zack Hersh <zackshersh@gmail.com>
2021-05-14 16:48:03 -0400

    signup form funcitonality added

--- public/js/login.js
+++ public/js/login.js
@@ -22,3 +22,30 @@ const loginFormHandler = async (event) => {
 document
   .querySelector('#login-form')
   .addEventListener('submit', loginFormHandler);
+
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#signup-username').value.trim();
+  const email = document.querySelector('#signup-email').value.trim();
+  const password = document.querySelector('#signup-password').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
