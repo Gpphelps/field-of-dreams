d7efc054980b23dd18443607efdc6771982e7667
Zack Hersh <zackshersh@gmail.com>
2021-05-14 16:59:21 -0400

    frontend backend signup issue fixed

--- public/js/login.js
+++ public/js/login.js
@@ -27,14 +27,14 @@ document
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#signup-username').value.trim();
+  const username = document.querySelector('#signup-username').value.trim();
   const email = document.querySelector('#signup-email').value.trim();
   const password = document.querySelector('#signup-password').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/users/', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
