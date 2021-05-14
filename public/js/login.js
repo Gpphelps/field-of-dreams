6be40486302147f57a9d0cf5d4b7b4277360fcfe
Zack Hersh <zackshersh@gmail.com>
2021-05-14 17:02:36 -0400

    name and username discontinuity in files fixed

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ const signupFormHandler = async (event) => {
   const email = document.querySelector('#signup-email').value.trim();
   const password = document.querySelector('#signup-password').value.trim();
 
-  if (name && email && password) {
+  if (username && email && password) {
     const response = await fetch('/api/users/', {
       method: 'POST',
       body: JSON.stringify({ username, email, password }),
