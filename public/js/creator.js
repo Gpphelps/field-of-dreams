57bb42eecdccb3e496938e77de41777348ca9a45
Zack Hersh <zackshersh@gmail.com>
2021-05-14 18:26:00 -0400

    solution for getting user-id readable to flower generator to send in with flower submission

--- public/js/creator.js
+++ public/js/creator.js
@@ -281,6 +281,8 @@ const submitFlower = async () => {
         petal_shape: JSON.stringify(attrObj.petalShape),
         petal_scale: attrObj.petalScale,
         petal_scale_variation: attrObj.petalScaleVariation,
+        user_id: 
+
     }
 
     statusP.innerHTML = 'Saving Flower...'
