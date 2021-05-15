79eefff56e6f0845e3e66debe9d727f1a85efb58
Mikey D <michaeldamero@gmail.com>
2021-05-15 12:50:18 -0400

    combo.js add

---
+++ public/js/combo.js
@@ -0,0 +1,15 @@
+document.querySelector('#newFlowerButton').addEventListener('mousedown',function(){
+    window.location.href = "/create"
+  })
+
+  document.querySelector('#profileButton').addEventListener('mousedown',function(){
+    window.location.href = "/profile"
+  })
+
+  document.querySelector('#login').addEventListener('mousedown',function(){
+    window.location.href = "/login"
+  })
+
+  document.querySelector('#maintitle').addEventListener('mousedown',function(){
+    window.location.href = "/"
+  })
